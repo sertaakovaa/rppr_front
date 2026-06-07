61ce5c6c3d930333256be486507c044b4ab69f91
sertaakovaa <sertaakovaa@gmail.com>
2026-06-08 00:39:19 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,18 +8,25 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      includeAssets: [
+        'favicon.ico',
+        'apple-touch-icon.png',
+        'masked-icon.svg',
+      ],
+
       manifest: {
         name: 'RPPR Front',
         short_name: 'RPPR',
         description: 'RPPR Frontend Application',
         theme_color: '#ffffff',
+
         icons: [
           {
             src: 'pwa-192x192.png',
             sizes: '192x192',
             type: 'image/png',
           },
+
           {
             src: 'pwa-512x512.png',
             sizes: '512x512',
@@ -27,19 +34,46 @@ export default defineConfig({
           },
         ],
       },
+
       workbox: {
         // globPatterns будет соответствовать всем файлам в папке dist
         globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
       },
     }),
   ],
+
   server: {
     proxy: {
-      '/auth': { target: 'http://localhost:8000', changeOrigin: true },
-      '/hotels': { target: 'http://localhost:8000', changeOrigin: true },
-      '/bookings': { target: 'http://localhost:8000', changeOrigin: true },
-      '/admin': { target: 'http://localhost:8000', changeOrigin: true },
-      '/room': { target: 'http://localhost:8000', changeOrigin: true },
+      '/auth': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+      },
+
+      '/hotels': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+      },
+
+      '/bookings': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+      },
+
+      '/admin': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+      },
+
+      '/room': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+      },
     },
   },
-})
+
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts',
+  },
+});
